a5b09ef37f987078e86d28b9b596af882c0fbf1d
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2023-03-24 19:15:50 -0500

    Inicio de proyecto Rest

---
+++ src/main/java/citasmedicas/BackendCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package citasmedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCitasMedicasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCitasMedicasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/citasmedicas/BackendCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package citasmedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCitasMedicasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
