52a8f89bc0cd45e01ffe534677010bf22017bc3d
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2023-04-08 22:18:46 -0500

    se agrego en endpoint de listar cita la documentacion faltante

--- src/main/java/citasmedicas/controller/CitaController.java
+++ src/main/java/citasmedicas/controller/CitaController.java
@@ -1,6 +1,12 @@
 package citasmedicas.controller;
 
+import citasmedicas.model.dto.CitaDTO;
 import citasmedicas.service.CitaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +23,12 @@ public class CitaController {
     @Autowired
     private CitaService service;
 
+    @Operation(description = "Lista las citas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    schema = @Schema(type = "array", implementation = CitaDTO.class))
+            )
+    })
     @GetMapping
     public ResponseEntity<?> listar() {
         return new ResponseEntity<>(service.listar(), HttpStatus.OK);
