1d4f012c43d892ddad0a7d2f0c49b6a0eb45fef8
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2024-07-10 11:22:43 -0500

    actualizacion de data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -12,15 +12,28 @@ CREATE TABLE clientes
 );
 
 INSERT INTO clientes
-    (id,
-     nombre,
-     apellido_paterno,
-     apellido_materno,
-     numero_documento,
-     fecha_nacimiento,
-     email,
-     telefono,
-     sexo)
+(id,
+ nombre,
+ apellido_paterno,
+ apellido_materno,
+ numero_documento,
+ fecha_nacimiento,
+ email,
+ telefono,
+ sexo)
 VALUES
     (1, 'Juan', 'Perez', 'Gomez', '12345678', '1990-01-01', 'juan@gmail.com', '12345678', 'M'),
     (2, 'Maria', 'Gomez', 'Perez', '87654321', '1995-01-01', 'maria@gmail.com', '87654321', 'F');
+
+CREATE TABLE areas
+(
+    id   INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    estado BOOLEAN
+);
+
+INSERT INTO areas
+    (id, nombre, estado) VALUES (1, 'Medicina General', TRUE),
+    (2, 'Traumatologia', TRUE),
+    (3, 'Ventas', TRUE);
+
