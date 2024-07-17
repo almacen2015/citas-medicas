cee0f9dc34e0e4cb18501d47c91d984b91c9abd1
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2024-07-17 10:14:49 -0500

    se arreglo manejo de excepciones en area

--- src/main/java/citasmedicas/ExceptionsAdvice.java
+++ src/main/java/citasmedicas/ExceptionsAdvice.java
@@ -21,8 +21,8 @@ public class ExceptionsAdvice {
     public ResponseEntity<?> handleClienteException(ClienteException e) {
         return switch (e.getMessage()) {
             case "NOMBRE_NO_VALIDO", "APELLIDO_PATERNO_NO_VALIDO",
-                    "APELLIDO_MATERNO_NO_VALIDO", "NUMERO_DOCUMENTO_NO_VALIDO",
-                    "FECHA_NACIMIENTO_NO_VALIDO", "SEXO_NO_VALIDO" ->
+                 "APELLIDO_MATERNO_NO_VALIDO", "NUMERO_DOCUMENTO_NO_VALIDO",
+                 "FECHA_NACIMIENTO_NO_VALIDO", "SEXO_NO_VALIDO" ->
                     new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
             case "CLIENTE_NO_ENCONTRADO" -> new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
             default -> new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -32,7 +32,8 @@ public class ExceptionsAdvice {
     @ExceptionHandler(AreaException.class)
     public ResponseEntity<?> handleAreaException(AreaException e) {
         return switch (e.getMessage()) {
-            case "NOMBRE_NO_VALIDO" -> new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            case "NOMBRE_NO_VALIDO", "ID_NO_VALIDO", "NOMBRE_EXISTE" ->
+                    new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
             default -> new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         };
     }
@@ -41,8 +42,8 @@ public class ExceptionsAdvice {
     public ResponseEntity<?> handleAreaException(CitaException e) {
         return switch (e.getMessage()) {
             case "EXISTE_CITA_MISMA_AREA", "EXISTE_CITA_MISMA_HORA",
-                    "TITULO_NO_VALIDO", "AREA_NO_VALIDO", "CLIENTE_NO_VALIDO",
-                    "commit", "FECHA_FIN_NO_VALIDO" -> new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+                 "TITULO_NO_VALIDO", "AREA_NO_VALIDO", "CLIENTE_NO_VALIDO",
+                 "commit", "FECHA_FIN_NO_VALIDO" -> new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
             default -> new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         };
     }
