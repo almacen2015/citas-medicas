0554a76fb96fdaefc89ecb33f66e70f825c0605b
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2024-07-20 14:29:19 -0500

    se agrego endpoint para actualizar tipo empleado

--- src/main/java/citasmedicas/controllers/TipoEmpleadoController.java
+++ src/main/java/citasmedicas/controllers/TipoEmpleadoController.java
@@ -3,6 +3,8 @@ package citasmedicas.controllers;
 import citasmedicas.models.dto.TipoEmpleadoDTO;
 import citasmedicas.services.TipoEmpleadoService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,31 +22,37 @@ public class TipoEmpleadoController {
 
     @Operation(summary = "Lista todos tipos de empleados", description = "Lista todos los tipos de empleados")
     @GetMapping("")
-    public List<TipoEmpleadoDTO> listar() {
-        return service.listar();
+    public ResponseEntity<List<TipoEmpleadoDTO>> listar() {
+        return new ResponseEntity<>(service.listar(), HttpStatus.OK);
     }
 
     @Operation(summary = "Guardar un tipo de empleado", description = "Recibe un tipo de empleado")
     @PostMapping("")
-    public TipoEmpleadoDTO guardar(@RequestBody TipoEmpleadoDTO tipoEmpleadoDTO) {
-        return service.guardar(tipoEmpleadoDTO);
+    public ResponseEntity<TipoEmpleadoDTO> guardar(@RequestBody TipoEmpleadoDTO tipoEmpleadoDTO) {
+        return new ResponseEntity<>(service.guardar(tipoEmpleadoDTO), HttpStatus.CREATED);
     }
 
     @Operation(summary = "Busca un tipo de empleado por su id", description = "Recibe el id de un tipo de empleado")
-    @GetMapping("/{id}")
-    public TipoEmpleadoDTO buscarPorId(@PathVariable Integer id) {
-        return service.buscarPorId(id);
+    @GetMapping("/buscar-id/{id}")
+    public ResponseEntity<TipoEmpleadoDTO> buscarPorId(@PathVariable Integer id) {
+        return new ResponseEntity<>(service.buscarPorId(id), HttpStatus.FOUND);
     }
 
     @Operation(summary = "Busca un tipo de empleado por su nombre", description = "Recibe el nombre de un tipo de empleado")
     @GetMapping("/buscar-nombre/{nombre}")
-    public TipoEmpleadoDTO buscarPorNombre(@PathVariable String nombre) {
-        return service.buscarPorNombre(nombre);
+    public ResponseEntity<TipoEmpleadoDTO> buscarPorNombre(@PathVariable String nombre) {
+        return new ResponseEntity<>(service.buscarPorNombre(nombre), HttpStatus.FOUND);
     }
 
     @Operation(summary = "Elimina un tipo de empleado", description = "Recibe un tipo de empleado")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Integer id) {
         service.eliminar(id);
+    }
+
+    @Operation(summary = "Actualiza un tipo de empleado", description = "Recibe un tipo de empleado")
+    @PutMapping("")
+    public ResponseEntity<TipoEmpleadoDTO> actualizar(@RequestBody TipoEmpleadoDTO tipoEmpleado) {
+        return new ResponseEntity<>(service.actualizar(tipoEmpleado), HttpStatus.OK);
     }
 }
