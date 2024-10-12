147d6cfc3e8f6f22000a42e423320f388662c919
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2024-10-12 11:41:27 -0500

    se comento codigo para crear usuarios

--- src/main/java/citasmedicas/BackendCitasMedicasApplication.java
+++ src/main/java/citasmedicas/BackendCitasMedicasApplication.java
@@ -1,17 +1,7 @@
 package citasmedicas;
 
-import citasmedicas.models.entities.Permiso;
-import citasmedicas.models.entities.Rol;
-import citasmedicas.models.entities.RolEnum;
-import citasmedicas.models.entities.Usuario;
-import citasmedicas.repositories.UsuarioRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
-import java.util.Set;
 
 @SpringBootApplication
 public class BackendCitasMedicasApplication {
@@ -20,76 +10,79 @@ public class BackendCitasMedicasApplication {
         SpringApplication.run(BackendCitasMedicasApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(UsuarioRepository usuarioRepository) {
-        return args -> {
-            Permiso createPermiso = Permiso.builder()
-                    .nombre("CREATE")
-                    .build();
-
-            Permiso readPermiso = Permiso.builder()
-                    .nombre("READ")
-                    .build();
-
-            Permiso updatePermiso = Permiso.builder()
-                    .nombre("UPDATE")
-                    .build();
-
-            Permiso deletePermiso = Permiso.builder()
-                    .nombre("DELETE")
-                    .build();
-
-            Rol rolAdmin = Rol.builder()
-                    .rolEnum(RolEnum.ADMIN)
-                    .permisos(Set.of(createPermiso, readPermiso, updatePermiso, deletePermiso))
-                    .build();
-
-            Rol rolDeveloper = Rol.builder()
-                    .rolEnum(RolEnum.DEVELOPER)
-                    .permisos(Set.of(createPermiso, readPermiso))
-                    .build();
-
-            Rol rolUser = Rol.builder()
-                    .rolEnum(RolEnum.USER)
-                    .permisos(Set.of(createPermiso, readPermiso, updatePermiso, deletePermiso))
-                    .build();
-
-            Rol rolInvited = Rol.builder()
-                    .rolEnum(RolEnum.INVITED)
-                    .permisos(Set.of(readPermiso))
-                    .build();
-
-            Usuario usuarioVictor = Usuario.builder()
-                    .username("victor")
-                    .password("$2a$10$mbkroRGK4o9feC0IBVZa9e8cBjxqDfyXyzYuvEwYTGGAqHh5FM/Bi")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialsNoExpired(true)
-                    .roles(Set.of(rolAdmin))
-                    .build();
-
-            Usuario usuarioLuis = Usuario.builder()
-                    .username("luis")
-                    .password("$2a$10$mbkroRGK4o9feC0IBVZa9e8cBjxqDfyXyzYuvEwYTGGAqHh5FM/Bi")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialsNoExpired(true)
-                    .roles(Set.of(rolDeveloper))
-                    .build();
-
-            Usuario usuarioMartin = Usuario.builder()
-                    .username("martin")
-                    .password("$2a$10$mbkroRGK4o9feC0IBVZa9e8cBjxqDfyXyzYuvEwYTGGAqHh5FM/Bi")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialsNoExpired(true)
-                    .roles(Set.of(rolUser))
-                    .build();
-
-            usuarioRepository.saveAll(List.of(usuarioVictor, usuarioMartin, usuarioLuis));
-        };
-    }
+//    @Bean
+//    CommandLineRunner init(UsuarioRepository usuarioRepository) {
+//        return args -> {
+//            Permiso createPermiso = Permiso.builder()
+//                    .nombre("CREATE")
+//                    .build();
+//
+//            Permiso readPermiso = Permiso.builder()
+//                    .nombre("READ")
+//                    .build();
+//
+//            Permiso updatePermiso = Permiso.builder()
+//                    .nombre("UPDATE")
+//                    .build();
+//
+//            Permiso deletePermiso = Permiso.builder()
+//                    .nombre("DELETE")
+//                    .build();
+//
+//            Rol rolAdmin = Rol.builder()
+//                    .rolEnum(RolEnum.ADMIN)
+//                    .permisos(Set.of(createPermiso, readPermiso, updatePermiso, deletePermiso))
+//                    .build();
+//
+//            Rol rolDeveloper = Rol.builder()
+//                    .rolEnum(RolEnum.DEVELOPER)
+//                    .permisos(Set.of(createPermiso, readPermiso))
+//                    .build();
+//
+//            Rol rolUser = Rol.builder()
+//                    .rolEnum(RolEnum.USER)
+//                    .permisos(Set.of(createPermiso, readPermiso, updatePermiso, deletePermiso))
+//                    .build();
+//
+//            Rol rolInvited = Rol.builder()
+//                    .rolEnum(RolEnum.INVITED)
+//                    .permisos(Set.of(readPermiso))
+//                    .build();
+//
+//            Usuario usuarioVictor = Usuario.builder()
+//                    .username("victor")
+//                    //1234
+//                    .password("$2a$10$mbkroRGK4o9feC0IBVZa9e8cBjxqDfyXyzYuvEwYTGGAqHh5FM/Bi")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialsNoExpired(true)
+//                    .roles(Set.of(rolAdmin))
+//                    .build();
+//
+//            Usuario usuarioLuis = Usuario.builder()
+//                    .username("luis")
+//                    //1234
+//                    .password("$2a$10$mbkroRGK4o9feC0IBVZa9e8cBjxqDfyXyzYuvEwYTGGAqHh5FM/Bi")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialsNoExpired(true)
+//                    .roles(Set.of(rolDeveloper))
+//                    .build();
+//
+//            Usuario usuarioMartin = Usuario.builder()
+//                    .username("martin")
+//                    //1234
+//                    .password("$2a$10$mbkroRGK4o9feC0IBVZa9e8cBjxqDfyXyzYuvEwYTGGAqHh5FM/Bi")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialsNoExpired(true)
+//                    .roles(Set.of(rolUser))
+//                    .build();
+//
+//            usuarioRepository.saveAll(List.of(usuarioVictor, usuarioMartin, usuarioLuis));
+//        };
+//    }
 }
